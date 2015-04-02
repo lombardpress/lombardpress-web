e948b212b9f071509f28f9157906280b90fe7fab
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2015-04-02 13:12:36 -0400

    possibily fixed earlier bug, which may have been caused by doubleRenderErr; fixed this by adding return after redirect and return if performed? after executing function

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -44,11 +44,16 @@ class TextController < ApplicationController
 		
 		item = get_item(params)
 		
-		check_permission(item)
+		check_permission(item); return if performed?
 		# need to fix database to include critical transcript as object of hasTranscription
 		# then uncomment below
 
-		check_transcript_existence(item, params)
+		# I don't quite understand the return if performed - got things to work following this http://blog.arkency.com/2014/07/4-ways-to-early-return-from-a-rails-controller/								
+		#check_transcript_existence(item, params); return if performed?
+		
+
+		
+		
 
 		@title = item.title
 		#remove @fs after check for use. use itemid instead
@@ -82,15 +87,15 @@ class TextController < ApplicationController
 		def check_permission(item)
 			if item.status == "In Progress" || item.status == "draft"
 				if current_user.nil?
-					redirect_to "/permissions#draftview", :alert => "Access denied: This text is a draft. It requires permission to be viewed."
+					redirect_to "/permissions#draftview", :alert => "Access denied: This text is a draft. It requires permission to be viewed." and return
 				elsif current_user.draft_reader? || current_user.draft_img_reader?
 					allowed_texts = current_user.texts.map {|text| text.itemid}
 					unless allowed_texts.include? params[:itemid]
-						redirect_to "/permissions#draftview", :alert => "Access denied: This text is a draft. It requires permission to be viewed."
+						redirect_to "/permissions#draftview", :alert => "Access denied: This text is a draft. It requires permission to be viewed." and return
 					end
 
 				elsif !current_user.admin? 
-					redirect_to "/permissions#draftview", :alert => "Access denied: This text is a draft. It requires permission to be viewed."
+					redirect_to "/permissions#draftview", :alert => "Access denied: This text is a draft. It requires permission to be viewed." and return
 				end
 			end
 		end
@@ -101,8 +106,8 @@ class TextController < ApplicationController
 		end
 		def check_transcript_existence(item, params)
 			wit = if params.has_key?(:msslug) then params[:msslug] else "critical" end
-			unless item.transcription?(wit)
-				redirect_to "/text/status/#{params[:itemid]}", :alert => "A critical/normalized edition of this text does not exist yet. Below are the available transcriptions."
+				unless item.transcription?(wit)
+					redirect_to "/text/status/#{params[:itemid]}", :alert => "A critical/normalized edition of this text does not exist yet. Below are the available transcriptions." and return
 			end
 		end
 end
