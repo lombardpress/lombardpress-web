daacd5818bba7d434bc21c8599bf0c1a520ee82b
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2015-04-11 12:11:33 -0400

    modified alpha invite mailer to include a link to a default host, in order to allow invite to direct to specified host

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,9 +4,10 @@ class UserMailer < ApplicationMailer
   	@url = "https://nameless-shelf-9637.herokuapp.com/users/sign_in"
   	mail(to: @user.email, subject: "Welcome to LomardPress")
   end
-  def alpha_invitation(user)
+  def alpha_invitation(user, host_link)
 
   	@user = user
-    mail(to: @user.email, from: 'jeffreycwitt@gmail.com', subject: "You've been invited to alpha test the new LombardPress2 rebuild")
+  	@host_link = host_link
+  	mail(to: @user.email, from: 'jeffreycwitt@gmail.com', subject: "You've been invited to alpha test the new LombardPress2 rebuild")
   end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,6 +2,6 @@
 class UserMailerPreview < ActionMailer::Preview
 	def alpha_invitation
 		#config = LbpConfig.new("plaoulcommentary.lombardpress.org")
-		UserMailer.alpha_invitation(User.find(3))
+		UserMailer.alpha_invitation(User.find(3), 'plaoulcommentary.lombardpress.org')
 	end
 end
