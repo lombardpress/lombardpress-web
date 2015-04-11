746d2255148e0bdd76f8b4aa50f48501fe6288c7
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2015-04-11 12:37:07 -0400

    added reset of mailer host depending on request in application controller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   #config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
+  config.action_mailer.default_url_options = {host: 'localhost', port: '3000'}
   config.action_mailer.smtp_settings = {
     :address   => ENV["MAIL_ADDRESS"], 
     :port      => ENV["MAIL_PORT"], # ports 587 and 2525 are also supported with STARTTLS
