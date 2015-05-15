0dbdd83100a889d918963e6751a2a1dd3f17fd74
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2015-05-15 15:43:59 -0400

    updated search ref to respond to new xquery file

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
 	def show
-		@results = open("http://localhost:8080/exist/rest/db/xQueries/plainTextSearch3.xql?param1=#{params[:searchterm]}").read
+		@results = open("http://localhost:8080/exist/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{@config.commentaryid}").read
 		
 	end
 end
