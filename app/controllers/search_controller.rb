0788b4f61e59733a21bbeeaa744575c8f4a97c6c
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2015-05-16 10:49:33 -0400

    [finished #94354410]; previous two commits

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,6 @@ class SearchController < ApplicationController
 	def show
 
 		#@results = open("http://localhost:8080/exist/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{@config.commentaryid}").read
-		
 		@results = open("http://sparql.scta.info:8080/exist/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{@config.commentaryid}", 
 			{:http_basic_authentication => [ENV['EXISTUN'], ENV['EXISTPW']]}).read
 		
