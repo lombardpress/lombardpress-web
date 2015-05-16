e56e1af00b9eb96b2fff00b83cd497b6429d477f
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2015-05-16 11:24:12 -0400

    added to_query method to query has to support escaping spaces

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,8 @@ class SearchController < ApplicationController
 	def show
 
 		#@results = open("http://localhost:8080/exist/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{@config.commentaryid}").read
+		param_hash = {query: params[:searchterm], commentarid: @config.commentaryid}.to_query
+		url = "http://sparql.scta.info:8080/exist/apps/scta/wordsearch.xq?" + param_hash
 		@results = open("http://sparql.scta.info:8080/exist/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{@config.commentaryid}", 
 			{:http_basic_authentication => [ENV['EXISTUN'], ENV['EXISTPW']]}).read
 		
