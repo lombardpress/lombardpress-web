2ac5e5770cbe01518180cb325b4f68b92df1fe3f
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2015-05-16 10:45:18 -0400

    configured search controller to use production exist db

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 class SearchController < ApplicationController
 	def show
-		@results = open("http://localhost:8080/exist/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{@config.commentaryid}").read
+		#@results = open("http://localhost:8080/exist/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{@config.commentaryid}").read
+		@results = open("http://sparql.scta.info:8080/exist/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{@config.commentaryid}", 
+			{:http_basic_authentication => [ENV['EXISTUN'], ENV['EXISTPW']]}).read
 		
 	end
 end
