ba8a576a09b95dcd2e222f2f14dd35b155581d73
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2015-07-18 21:59:19 -0400

    side window popup functions now also try to re-adjust position of window after text width has been reduced and shifted. It works ok, but is a rough estimate so it isn't always perfect

--- app/assets/javascripts/side_window.js
+++ app/assets/javascripts/side_window.js
@@ -4,58 +4,83 @@ $(document).on('ready page:load', function () {
 	$(document).ready(function(){
 
 		$("a.js-show-outline").click(function(){
-			showSideWindow();
+			$paragraph = getCurrentViewingParagraph()
+			showSideWindow($paragraph);
 			var itemid = $(this).attr("data-itemid");
 			showOutline(itemid);
 		});
 		$("a.js-close-side-window").click(function(){
-			hideSideWindow();
+			$paragraph = getCurrentViewingParagraph()
+			hideSideWindow($paragraph);
 		});
 		$("a.js-minimize-side-window").click(function(){
-			minimizeSideWindow();
+			$paragraph = getCurrentViewingParagraph()
+			minimizeSideWindow($paragraph);
 		});
 		$("a.js-maximize-side-window").click(function(){
-			maximizeSideWindow();
+			$paragraph = getCurrentViewingParagraph()
+			maximizeSideWindow($paragraph);
 		});
 	});
 });
 
 
+var scrollToParagraph = function(element){
+	if (element.length > 0) {
+	    $('html, body')
+            .stop()
+            .animate({
+                scrollTop: element.offset().top - 200
+            }, 1000);
+   }
 
+}
 
+var getCurrentViewingParagraph = function(){
+	var $paragraphs = $('p.plaoulparagraph');
+	var viewerMidPoint = $(window).height()/2
+	console.log(viewerMidPoint);
+	$paragraph = ($paragraphs.nearest({y: $(window).scrollTop() + viewerMidPoint, x: 0})); 
+	console.log($paragraphs.nearest({y: $(window).scrollTop() + viewerMidPoint, x: 0}));
+	return $paragraph
+}
 
-var showSideWindow = function(){
+var showSideWindow = function(element){
+	
 	$("div#lbp-text-body").animate({"width": "50%", "margin-left": "45%"}, function(){
 		$("div#lbp-side-window").css("display", "block").promise().done(function(){
-			$("div#lbp-side-window").animate({"width": "40%"});	
+			$("div#lbp-side-window").animate({"width": "40%"}, scrollToParagraph(element));
 		});
 	});
+	
 };
 
-var hideSideWindow = function(){
+var hideSideWindow = function(element){
+	
 	$("div#lbp-side-window").animate({"width": "0"}, function(){
 		$("div#lbp-side-window").css("display", "none").promise().done(function(){
 			$("div#lbp-text-body").css({"margin-left": "auto"}).promise().done(function(){
-				$("div#lbp-text-body").css({"width": "60%"});
+				$("div#lbp-text-body").css({"width": "60%"}).promise().done(scrollToParagraph(element));
 			});
 		});
 	});
 };
 
-var minimizeSideWindow = function(){
+var minimizeSideWindow = function(element){
+	
 	$("li#lbp-max-side-window").css("display", "block").promise().done(function(){
 		$("li#lbp-min-side-window").css("display", "none").promise().done(function(){
 			$("div#lbp-side-window").animate({"width": "100px"}, function(){
 				$("div#lbp-text-body").css({"margin-left": "auto"}).promise().done(function(){
-					$("div#lbp-text-body").css({"width": "60%"});	
+					$("div#lbp-text-body").css({"width": "60%"}).promise().done(scrollToParagraph(element));	
 				}); 
 			}); 
 		});
 	});
 };
-var maximizeSideWindow = function(){
+var maximizeSideWindow = function(element){
 	$("li#lbp-min-side-window").css("display", "block").promise().done(function(){
-			$("li#lbp-max-side-window").css("display", "none").removeClass("js-maximize-side-window").promise().done(showSideWindow);	
+			$("li#lbp-max-side-window").css("display", "none").removeClass("js-maximize-side-window").promise().done(showSideWindow(element));	
 		});
 };
 
