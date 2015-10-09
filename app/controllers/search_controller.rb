d9eaf9f6b9231bcc77f9eb12983bb84cd2360188
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2015-10-09 11:37:01 -0400

    modified search controller to use existdb rest api without need for basic auth

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,8 +4,8 @@ class SearchController < ApplicationController
 		#@results = open("http://localhost:8080/exist/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{@config.commentaryid}").read
 		param_hash = {query: params[:searchterm], commentarid: @config.commentaryid}.to_query
 		url = "http://sparql.scta.info:8080/exist/apps/scta/wordsearch.xq?" + param_hash
-		@results = open("http://sparql.scta.info:8080/exist/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{@config.commentaryid}", 
-			{:http_basic_authentication => [ENV['EXISTUN'], ENV['EXISTPW']]}).read
+		@results = open("http://exist.scta.info/exist/rest/db/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{@config.commentaryid}").read 
+			#{:http_basic_authentication => ["reader", "READER"]}).read
 		
 	end
 end
