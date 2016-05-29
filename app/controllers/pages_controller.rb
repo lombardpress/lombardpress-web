9befaeab739e211ff1eadbb62906431b12792f02
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2016-05-29 18:43:24 -0400

    more bug fixes landing page re-directs and broken links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,15 +3,18 @@ class PagesController < ApplicationController
 	
 	def home
 
-		if request.host.include? "petrusplaoul"
-          redirect_to "/text/questions/plaoulcommentary"
-    elsif request.host.include? "adamwodeham"
-      redirect_to "/text/questions/wodehamordinatio"
-    elsif request.subdomains.any?
-    	redirect_to "/text/questions/#{request.subdomains.first}" 
-    else
-			render :home
+		unless request.host.include? "scta" or request.host.include? "scta-staging"
+			if request.host.include? "petrusplaoul"
+	          redirect_to "/text/questions/plaoulcommentary"
+	    elsif request.host.include? "adamwodeham"
+	      redirect_to "/text/questions/wodehamordinatio"
+	    elsif request.subdomains.any?
+	    	redirect_to "/text/questions/#{request.subdomains.first}" 
+	    else
+				render :home
+			end
 		end
+
 	end
 	
 	def permissions
