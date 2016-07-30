7eed000b4fd05c1901d72b5c3c2e5fe1aa550140
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2016-07-30 12:34:21 -0400

    commented out module/lbp/expression which seemed to be causing error in heroku deploy; return xslt/1.0.0/documentary to symlink status

--- app/models/lbp/expression.rb
+++ app/models/lbp/expression.rb
@@ -1,7 +1,7 @@
-module Lbp
-	class Expression.class_eval
-		def overview
-			Lbp::Query.new().collection_query(url)
-		end
-	end
-end
+#module Lbp
+#	class Expression.class_eval
+#		def overview
+#			Lbp::Query.new().collection_query(url)
+#		end
+#	end
+#end
