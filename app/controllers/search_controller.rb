d73693a348fa76b4be4f60fedf3ee95366ea5b12
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2017-04-13 14:24:19 -0400

    updated search controller to use new search url

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,14 @@
 class SearchController < ApplicationController
 	def show
-		if params[:use] == "sparql"
+		if params[:use] == "expressiontype"
 			expressionid = params[:expressionid] ? params[:expressionid] : "all"
-			@results = open("http://exist.scta.info/exist/rest/db/apps/scta/search-with-sparql.xq?query=#{params[:searchterm]}&expressionid=#{expressionid}").read
+			@results = open(open("http://exist.scta.info/exist/apps/scta-app/search/expressiontype/#{params[:expressionid]}/#{params[:searchterm]}")).read
 		elsif params[:use] == "author"
 			@results = open("http://exist.scta.info/exist/apps/scta-app/search/author/#{params[:authorid]}/#{params[:searchterm]}").read
 		else
-		expressionid = params[:expressionid] ? params[:expressionid] : "all"
-		#@results = open("http://localhost:8080/exist/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{@config.commentaryid}").read
-		#param_hash = {query: params[:searchterm], commentaryid: @config.commentaryid}.to_query
-		#url = "http://sparql.scta.info:8080/exist/apps/scta/wordsearch.xq?query" + param_hash[]
-		@results = open("http://exist.scta.info/exist/rest/db/apps/scta/wordsearch.xq?query=#{params[:searchterm]}&commentaryid=#{expressionid}").read
-		#@results = open(url).read
-			#{:http_basic_authentication => ["reader", "READER"]}).read
-		end
+			expressionid = params[:expressionid] ? params[:expressionid] : "all"
+			@results = open("http://exist.scta.info/exist/apps/scta-app/search/expression/#{expressionid}/#{params[:searchterm]}").read
+		end 
 
 
 	end
