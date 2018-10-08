db458ea796c4777756cce5934b98c44ab96c52be
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2018-10-09 00:33:44 +0200

    hard coded render view in error controler to deal with png format error coming from apple-icon request

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,11 @@
 class ErrorsController < ApplicationController
   def not_found
-  	render(status: 404)
+  	#render(status: 404)
+    render "not_found.html.erb"
   end
 
   def internal_server_error
-  	render(status: 500)
+  	#render(status: 500)
+    render "internal_server_error.html.erb"
   end
 end
